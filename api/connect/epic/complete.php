b13b9c56c4674654b8cbc81ed49d4ae66e44ec54
flare576 <flare576@gmail.com>
2025-12-29 18:59:40 -0600

    fix(php): correct file depth

--- api/connect/epic/complete.php
+++ api/connect/epic/complete.php
@@ -1,4 +1,4 @@
 <?php
 // Forward to the main connect.php handler
 $_SERVER['REQUEST_URI'] = '/api/connect/epic/complete';
-require_once __DIR__ . '/../connect.php';
+require_once __DIR__ . '/../../connect.php';
